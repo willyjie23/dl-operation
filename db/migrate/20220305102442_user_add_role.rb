e3bd6823fea4dae8b9d4618dffdffae507e21848
willychang <a0903084107@gmail.com>
2022-03-05 18:30:50 +0800

    user add role column

---
+++ db/migrate/20220305102442_user_add_role.rb
@@ -0,0 +1,5 @@
+class UserAddRole < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :role, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_093104) do
+ActiveRecord::Schema.define(version: 2022_03_05_102442) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_03_05_093104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
